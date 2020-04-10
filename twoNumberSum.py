35e1121e626e85f307a3c708610b815a3eacfc48
Damilare Lana <damilarelana@gmail.com>
2020-04-10 06:40:40 +0100

    refactored to use a list comprehension

--- twoNumberSum.py
+++ twoNumberSum.py
@@ -1,13 +1,18 @@
 import time
 
 # define twoNumberSum function
+# * specifies format of user defined array
+# * calls the input() method to get user input
+# * calls inputArrayStringParser() to convert users `string` to `integer array`
+# * returns the inputArray
 
 
-def twoNumberSum(inputList: list):  # inputList param is of type list
+def twoNumberSum():  # inputList param is of type list
     print("Use array format [1, 2, 3, 4, 5] \n")
     inputArrayString = input("Enter an integer array: ")
     inputArray = inputArrayStringParser(inputArrayString)
     return inputArray
+
 
 # define arrayInput function
 # * uses raw_input()
@@ -21,10 +26,13 @@ def inputArrayStringParser(inputString: str):  # inputString is of type string
     inputString = inputString[1:]  # strip left bracket : 'index 0'
     lastIndex = len(inputString) - 1
     inputString = inputString[: lastIndex]  # strip right bracket : last Index
-    inputList = inputString.split()  # get elements using delimiter ", "
-    return inputList  # return the integer array
+    listOfString = inputString.split(", ")  # get elements using delimiter ", "
+    listOfInt = [int(x) for x in listOfString]  # using list comprehension
+    return listOfInt  # return the integer array
 
 
 start_time = time.time()
-print("Input Array: %" % twoNumberSum())
+inputArray = twoNumberSum()
+print("Input Array: ", inputArray)
+print("Array is of type: ", type(inputArray))
 print("Time: %f seconds" % (time.time() - start_time))
