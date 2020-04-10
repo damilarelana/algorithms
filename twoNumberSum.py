68d566b6646344ff6f83b881c8d2706548897cb7
Damilare Lana <damilarelana@gmail.com>
2020-04-10 21:48:15 +0100

    renamed inputArrayStringParser() to parseInputArrayString(). added getInteger(). added getArray(). refactored twoNumberSum

--- twoNumberSum.py
+++ twoNumberSum.py
@@ -1,31 +1,77 @@
 import time
 
 # define twoNumberSum function
-# - specifies format of user defined array
-# - calls the input() method to get user input
-# - calls inputArrayStringParser() to convert users `string` to `integer array`
-# - returns the inputArray
+# - takes in a user defined integer array
+# - takes in a user defined integer value
+# - checks if any two elements of array sums up to integer value
+# - saves all 2-element `[a, b]` pairs that sum up to integer value, in a dict
+# - returns
+#       + an empty `[]`, when no 2-element pairs sums up to integer value
+#       + a dict of all 2-element `[a, b]` pairs that sum up to integer value
 
 
 def twoNumberSum():  # inputList param is of type list
-    print("Using the format [1, 2, 3, 4] or [1,2,3,4] or [1, 2,3, 4] ...")
-    inputArrayString = input("please enter an integer array: ")
-    while checkEmptyList(inputArrayString):
-        print("Array cannot be empty")
-        inputArrayString = input("Enter an integer array: ")
-    inputArray = inputArrayStringParser(inputArrayString)
+
+    # obtain test array
+    testArray = getArray()
+    print("Test Array: ", testArray)
+    print("Test Array is of type: ", type(testArray))
+
+    # obtain test integer
+    testInteger = getInteger()
+    print("Test Sum: ", testInteger)
+    print("Sum is of type: ", type(testInteger))
+
+# define getArray()
+# - uses a try-except-finally to catch input edge-cases
+# - specifies format of user defined array
+# - calls the input() to get user defined array
+# - calls parseInputArrayString() to convert users `string` to `integer array`
+# - returns the user defined integer array value [when all goes to plan]
+
+
+def getArray():
+    try:
+        print("Using the format [1, 2, 3, 4] or [1,2,3,4] or [1, 2,3, 4] ...")
+        inputArrayString = input("please enter an integer array: ")
+        while checkEmptyList(inputArrayString):
+            print("Array cannot be empty")
+            inputArrayString = input("Enter an integer array: ")
+        inputArray = parseInputArrayString(inputArrayString)
+    except ValueError:
+        raise Exception("Unable to initialize the user defined array")
+    finally:
+        print("Successfully initialized the *user defined integer array*")
     return inputArray
 
-
-# define arrayInput function
-# - uses raw_input()
-# - to extract/convert a user defined input string into an array
-# - without using the list() constructor
-# - without entering the numbers 1 by one
-# - allowing using to enter the array in the format "[1, 2, 3, 4, 5, 6]
+# define getInteger()
+# - uses a try-except-finally to catch input edge-cases
+# - calls input() to get user defined integer value
+# - returns the user defined integer value [when all goes accordingly to plan]
 
 
-def inputArrayStringParser(inputString: str):  # inputString is of type string
+def getInteger():
+    try:
+        inputInteger = input("please enter an integer value: ")
+        while isinstance(inputInteger, int) is False:
+            print("Input must be an integer")
+            inputInteger = input("please enter an integer value: ")
+    except ValueError:
+        raise Exception("Unable to initialize the user defined integer value")
+    finally:
+        print("Successfully initialized the *user defined integer value*")
+
+    return inputInteger
+
+# define parseInputArrayString() function
+# - assumes user leveraged string format: [1, 2, 3, 4] or [1,2,3,4] or [1, 2,3, 4] ...
+# - strips away the left and right brackets
+# - strips away the extra whitespaces around each element  input()
+# - converts each element from a string to an int
+# - returns an integer array
+
+
+def parseInputArrayString(inputString: str):  # inputString is of type string
     parsedString = stringStripper(inputString)
     listOfString = parsedString.split(",")  # get elements using delimiter ","
     listOfString = [s.strip() for s in listOfString]  # remove whitespaces around each element
@@ -69,9 +115,7 @@ def checkEmptyList(inputArrayString: str):
 
 def main():
     start_time = time.time()
-    inputArray = twoNumberSum()
-    print("Input Array: ", inputArray)
-    print("Array is of type: ", type(inputArray))
+    twoNumberSum()
     print("Time: %f seconds" % (time.time() - start_time))
 
 
