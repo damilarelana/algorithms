da5181408e0860277c23f6fd341c25a46256fb57
Damilare Lana <damilarelana@gmail.com>
2020-04-10 08:03:22 +0100

    added checkEmptyList function

--- twoNumberSum.py
+++ twoNumberSum.py
@@ -1,25 +1,28 @@
 import time
 
 # define twoNumberSum function
-# * specifies format of user defined array
-# * calls the input() method to get user input
-# * calls inputArrayStringParser() to convert users `string` to `integer array`
-# * returns the inputArray
+# - specifies format of user defined array
+# - calls the input() method to get user input
+# - calls inputArrayStringParser() to convert users `string` to `integer array`
+# - returns the inputArray
 
 
 def twoNumberSum():  # inputList param is of type list
     print("Use array format [1, 2, 3, 4, 5] \n")
     inputArrayString = input("Enter an integer array: ")
+    while checkEmptyList(inputArrayString):
+        print("Array cannot be empty")
+        inputArrayString = input("Enter an integer array: ")
     inputArray = inputArrayStringParser(inputArrayString)
     return inputArray
 
 
 # define arrayInput function
-# * uses raw_input()
-# * to extract/convert a user defined input string into an array
-# * without using the list() constructor
-# * without entering the numbers 1 by one
-# * allowing using to enter the array in the format "[1, 2, 3, 4, 5, 6]
+# - uses raw_input()
+# - to extract/convert a user defined input string into an array
+# - without using the list() constructor
+# - without entering the numbers 1 by one
+# - allowing using to enter the array in the format "[1, 2, 3, 4, 5, 6]
 
 
 def inputArrayStringParser(inputString: str):  # inputString is of type string
@@ -30,9 +33,9 @@ def inputArrayStringParser(inputString: str):  # inputString is of type string
 
 
 # define stringStripper() function
-#  * takes in an input string in an array format [1, 2, 3, 4]
-#  * strips away the brackets
-#  * returns a bare parsed string
+#  - takes in an input string in an array format [1, 2, 3, 4]
+#  - strips away the brackets
+#  - returns a bare parsed string
 
 
 def stringStripper(inputArrayString: str):
@@ -42,18 +45,26 @@ def stringStripper(inputArrayString: str):
     return parsedArrayString
 
 # define checkEmptyList() function
-# * takes in an input string in an array format [1, 2, 3, 4]
-# * checks if it is empty []
-# * checks if it is [, ] or [,] or [ ,] or [ , ]
-# * returns a boolean true or false
+# - takes in an input string in an array format [1, 2, 3, 4]
+# - uses python dictionary to simulate a switch
+# - checks if it is empty string ""
+# - checks if it is string "[]"" or "[, ]"" or "[,]"" or "[ ,]"" or "[ , ]"
+# - returns a True (i.e. an empty list) or otherwise it returns a False
 
 
 def checkEmptyList(inputArrayString: str):
-    return True
+    return {
+        "[]": True,
+        "[, ]": True,
+        "[,]": True,
+        "[ ,]": True,
+        "[ , ]": True,
+        "": True,
+    }.get(inputArrayString, False)
 
 
 # define main() function
-# * to allow code to be run as either standalone or re-usable code
+# - to allow code to be run as either standalone or re-usable code
 
 def main():
     start_time = time.time()
