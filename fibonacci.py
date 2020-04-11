dba6a00d8141181f254918a95c103e12bd50e85f
Damilare Lana <damilarelana@gmail.com>
2020-04-11 21:40:34 +0100

    refactored fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,12 @@
-import math
 import time
 
-#define nestedSum function
+# define nestedSum function
+
+
 def fibonnaci(testnumber):
     if testnumber in [0, 1]:
         fibonnaciValue = testnumber
+        return fibonnaciValue
     elif testnumber < 0:
         testnumber = int(input("Enter a non-negative sequence index: "))
         print("New sequence index: %d" % testnumber)
@@ -17,7 +19,7 @@ def fibonnaci(testnumber):
 
 # call functions
 start_time = time.time()
-testfigure = 10
+testfigure = 8
 print("Fibonacci value is: %d" % fibonnaci(testfigure))
 print("Time: %f seconds" % (time.time() - start_time))
 print("Initial Sequence index: %s" % testfigure)
