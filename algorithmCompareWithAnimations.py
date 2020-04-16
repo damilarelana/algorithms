140670b44766c584d8b7e4b0450fd5f200f8cb24
Damilare Lana <damilarelana@gmail.com>
2020-04-16 05:26:24 +0100

    cleaned up the plotting area

--- algorithmCompareWithAnimations.py
+++ algorithmCompareWithAnimations.py
@@ -6,10 +6,8 @@ import pdb
 import secrets
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as FuncAnimation
 plt.style.use('seaborn-pastel')
 plt.style.use('dark_background')
-
 #
 # Generate Random Unsorted List
 #
@@ -281,8 +279,13 @@ def parsePlotData(plotDataDict: dict):
 
 
 def createAnimation(stateDataLists: list, listMinValue: int, listMaxValue: int, algorithmName: str, animationFormat: str):
+
     # setup the matplotlib's plot parameters
-    fig, arrayPlot = setupPlotParams(listMinValue, listMaxValue, stateDataLists)
+    fig, ax, alpha, color = setupPlotParams(listMinValue, listMaxValue)
+
+    # setup the mesh grid
+    yLinspace = np.linspace(listMinValue, listMaxValue, inputListLength) # creates even space in y-axis for array element values
+    xx, yy = np.meshgrid(xLinspace, yLinspace)  # create the mesh grid 
 
     # create animation
     #   - 'interval' talks about ms interval between frames
@@ -290,67 +293,79 @@ def createAnimation(stateDataLists: list, listMinValue: int, listMaxValue: int, 
     #   - 'blit' ensures that only areas of the plot which have changed are re-drawn i.e. improves performance and smoothness
     #   - uses `fig` after setupPlotParams() is called
     #   - calls `animate()` [while using the `stateDataList` and `arrayPlot`] defined within the scope of `createAnimation`
-    animation = FuncAnimation(fig, animate(arrayPlot, stateDataLists), interval=200, frames=inputListLength-1, blit=True)
 
-    # show
-    plt.draw()
-    plt.show()
+    # numEvents
+    #   - refers to the number of stateData that exists in the stateDataLists
+    numEvents = len(stateDataLists)
 
+    # plot the first array data
+    #   - note that `stateDataLists[0, :]` is acting like `yy` i.e. the height data to the barplot handler
+    #   - hence why we later `animate` i.e. iterate of `yy` (i.e. stateDataLists[i, :] different indices) in the `animate()` function
+    print("ax: ", ax)
+    print("fig: ", fig)
+    print("yy: ", yy)
+    print("yy[0]: ", yy[0])
+    print("xx: ", xx)
+    print("xx[0]: ", xx[0])
+    print("numEvents: ", numEvents)
+    print("stateDataLists: ", stateDataLists)
+    print("stateDataLists[0]: ", stateDataLists[0])
+    pdb.set_trace()
+    arrayPlot = ax.bar(xx[0], stateDataLists[0], color, alpha)  # helps to ensure that we plot only the first array due to how ax.bar handles 
+    animate(arrayPlot, stateDataLists, fig, numEvents)
+    
     # save animation
     #   - checks the animation format
     #   - uses anonymous function to simulate a switch statement
     #   - to save as `mp4` change `filename` assignment i.e. "filename = algorithm + '.mp4' "
     filename = algorithmName + animationFormat
-    if animationFormat == "gif":
-        animation.save(filename, writer='imagemagick')
-    else:
-        animation.save(filename)
+    # if animationFormat == "gif":
+    #     animation.save(filename, writer='imagemagick')
+    # else:
+    #     animation.save(filename)
 
 
 # animate()
-#  -
-# def animate(i: int, xx, stateDataLists, ax):
-def animate(i: int, arrayPlot, stateDataLists):
-    arrayPlot.set_height(stateDataLists[i])
-    # arrayPlot = ax.bar(xx[0], stateDataLists[i], color='green', alpha=0.8)  # `[0]` helps to ensure that we plot only the first array due to how ax.bar handles 
-    return arrayPlot
+def animate(arrayPlot, stateDataLists, fig, numEvents):
+    for i in range(numEvents):
+        arrayPlot.set_height(stateDataLists[i])
+        fig.canvas.draw()
+        fig.canvas.flush_events()
 
 # setupPlotParameters()
 # - sets up the required Matplotlib parameters
+def setupPlotParams(listMinValue: int, listMaxValue: int):
 
-
-def setupPlotParams(listMinValue: int, listMaxValue: int, stateDataLists: list):
+    plt.ion()
     # setup plotting area attributes
     #   - set_ylim refers to the the actual values in the array hence why it should be on the y-axis - in relation to a bar chart
     #       + `5` is used to pad the listMinValue and listMaxValue to animating those values easier to see
     #   - set_xlim refers to the index numbers of the array, which can be computed simply from the array length 
     #   - xLinspace is obtained from the global variable scope
     #   - yLinspace is calculated here using attributes of the already sorted list e.g. listMaxValue
-
-    yLinspace = np.linspace(listMinValue, listMaxValue, inputListLength) # creates even space in y-axis for array element values
-    xx, yy = np.meshgrid(xLinspace, yLinspace)  # create the mesh grid 
-
     # figsize = (6, 3)  # set figure size tuple i.e. canvas size (i.e. paper size: A4, Letter, wide-screen aspect ratio etc.)
-    fig, ax = plt.subplots(figsize=(6, 3))
+    fig, ax = plt.subplots(figsize=(10, 6))
     # ax.set(xlim=(0, inputListLength-1), ylim=(listMinValue-5, listMaxValue+5))
     ax.set_xlim(0, inputListLength-1)  # handles the scaling of the axis
-    ax.set_ylim(0, listMinValue-5, listMaxValue+5)
+    ax.set_ylim(listMinValue-5, listMaxValue+5)
 
     ax.set_xlabel("Index")  # handles the title label for of x-axis
     ax.set_ylabel("Value")  # handles the title label for of y-axis
 
     ax.set_title(algorithmName)  # handles printing of the name of the overall plot at the top
-    
-    color='green' # color of each bar chart shape
+    ax.legend(frameon=False)
 
-    pdb.set_trace()
+    # ticks customization
+    ax.tick_params(colors='gray', direction='out') 
 
-    # plot the first array data
-    #   - note that `stateDataLists[0, :]` is acting like `yy` i.e. the height data to the barplot handler
-    #   - hence why we later `animate` i.e. iterate of `yy` (i.e. stateDataLists[i, :] different indices) in the `animate()` function
-    arrayPlot = ax.bar(xx[0], stateDataLists[0], color, alpha=0.8)  # `[0]` helps to ensure that we plot only the first array due to how ax.bar handles 
+    color='#EE6666' # color of each bar chart shape
+    edgecolor='#E6E6E6'
+    alpha=0.6 # transparency
+
+    plt.grid(color='gray', linestyle='solid')
+
     
-    return fig, arrayPlot  # return generated plot setup parameters as a tuple
+    return fig, ax, alpha, color
 
 
 
--- scrapcode.py
+++ scrapcode.py
@@ -1,14 +1,92 @@
-    # initialize dict, with `index 0` being an empty list
-    keyDict = 0  # key to store the matching pairs (indexTuple, valueTuple)
-    resultPair = list()  # initialize placeholder for matched pairs
-    plotDataDict = { 
-        keyDict: resultPair,
-    }
+
+# animate()
+#  -
+# def animate(i: int, xx, stateDataLists, ax):
+def animate(i: int, arrayPlot, stateDataLists):
+    arrayPlot.set_height(stateDataLists[i])
+    # arrayPlot = ax.bar(xx[0], stateDataLists[i], color='green', alpha=0.8)  # `[0]` helps to ensure that we plot only the first array due to how ax.bar handles 
+    return arrayPlot
+
+
+
+def setupPlotParams(listMinValue: int, listMaxValue: int, stateDataLists: list):
+    # setup plotting area attributes
+    #   - set_ylim refers to the the actual values in the array hence why it should be on the y-axis - in relation to a bar chart
+    #       + `5` is used to pad the listMinValue and listMaxValue to animating those values easier to see
+    #   - set_xlim refers to the index numbers of the array, which can be computed simply from the array length 
+    #   - xLinspace is obtained from the global variable scope
+    #   - yLinspace is calculated here using attributes of the already sorted list e.g. listMaxValue
+
+    yLinspace = np.linspace(listMinValue, listMaxValue, inputListLength) # creates even space in y-axis for array element values
+    xx, yy = np.meshgrid(xLinspace, yLinspace)  # create the mesh grid 
+
+    # figsize = (6, 3)  # set figure size tuple i.e. canvas size (i.e. paper size: A4, Letter, wide-screen aspect ratio etc.)
+    fig, ax = plt.subplots(figsize=(6, 3))
+    # ax.set(xlim=(0, inputListLength-1), ylim=(listMinValue-5, listMaxValue+5))
+    ax.set_xlim(0, inputListLength-1)  # handles the scaling of the axis
+    ax.set_ylim(0, listMinValue-5, listMaxValue+5)
+
+    ax.set_xlabel("Index")  # handles the title label for of x-axis
+    ax.set_ylabel("Value")  # handles the title label for of y-axis
+
+    ax.set_title(algorithmName)  # handles printing of the name of the overall plot at the top
+    
+    color='green' # color of each bar chart shape
+
+    pdb.set_trace()
+
+    # plot the first array data
+    #   - note that `stateDataLists[0, :]` is acting like `yy` i.e. the height data to the barplot handler
+    #   - hence why we later `animate` i.e. iterate of `yy` (i.e. stateDataLists[i, :] different indices) in the `animate()` function
+    arrayPlot = ax.bar(xx[0], stateDataLists[0], color, alpha=0.8)  # `[0]` helps to ensure that we plot only the first array due to how ax.bar handles 
+    
+    return fig, arrayPlot  # return generated plot setup parameters as a tuple
 
 
 
 
+def createAnimation(stateDataLists: list, listMinValue: int, listMaxValue: int, algorithmName: str, animationFormat: str):
+    # setup the matplotlib's plot parameters
+    fig, arrayPlot = setupPlotParams(listMinValue, listMaxValue, stateDataLists)
 
-def getArrayPlotData(arrayWhileSorting: list, plotDataDict: dict):
-    cStateOfArray = [(index, value) for index, value in enumerate(arrayWhileSorting)]  # generate current state of the array
-    indexTuple, valueTuple = zip(*cStateOfArray)
+    # create animation
+    #   - 'interval' talks about ms interval between frames
+    #   - 'frames' required to help save the animation later
+    #   - 'blit' ensures that only areas of the plot which have changed are re-drawn i.e. improves performance and smoothness
+    #   - uses `fig` after setupPlotParams() is called
+    #   - calls `animate()` [while using the `stateDataList` and `arrayPlot`] defined within the scope of `createAnimation`
+    animation = FuncAnimation(fig, animate(arrayPlot, stateDataLists), interval=200, frames=inputListLength-1, blit=True)
+
+    # show
+    plt.draw()
+    plt.show()
+
+    # save animation
+    #   - checks the animation format
+    #   - uses anonymous function to simulate a switch statement
+    #   - to save as `mp4` change `filename` assignment i.e. "filename = algorithm + '.mp4' "
+    filename = algorithmName + animationFormat
+    if animationFormat == "gif":
+        animation.save(filename, writer='imagemagick')
+    else:
+        animation.save(filename)
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 6*np.pi, 100)
+y = np.sin(x)
+
+# You probably won't need this if you're embedding things in a tkinter plot...
+plt.ion()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+line1, = ax.plot(x, y, 'r-') # Returns a tuple of line objects, thus the comma
+
+for phase in np.linspace(0, 10*np.pi, 500):
+    line1.set_ydata(np.sin(x + phase))
+    fig.canvas.draw()
+    fig.canvas.flush_events()
