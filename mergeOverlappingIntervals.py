4d6ac61c15bafa60985ad2b719705c909d15511e
Damilare Lana <damilarelana@gmail.com>
2020-12-22 10:20:00 +0100

    finished updating all getattr() and setattr() scenarios to obtain object attribute values

--- mergeOverlappingIntervals.py
+++ mergeOverlappingIntervals.py
@@ -22,8 +22,8 @@ def mergeIntervals(testList: list):
 
     newList = list() # initialize a new array
     newList.append( Interval( # start pre-populating new array with a new first pair as if it is first correct state 
-        testList[0].start, # extract start value of first interval and pass it here to create a new interval for newArray
-        testList[0].end # extract end value of first interval and pass it here to create a new interval for newArray
+        getattr(testList[0], 'start'), # extract start value of first interval and pass it here to create a new interval for newArray
+        getattr(testList[0], 'end') # extract end value of first interval and pass it here to create a new interval for newArray
     ))
 
     # start iterating over the remaining intervals object in testList
@@ -34,8 +34,8 @@ def mergeIntervals(testList: list):
             setattr(newList[lastIndexNewList], 'end', max(testList[i].end, newList[lastIndexNewList].end)), # since there is an overlap, then choose largest endtimestamp (between current last interval [of newList] and current test interval of testList)
         else: # if there is no overlap, then just append the current test interval object to the newlist
             newList.append( Interval( 
-                testList[i].start,
-                testList[i].end
+                getattr(testList[i], 'start'),
+                getattr(testList[i], 'end')
             ))
     return newList # return the now populated newList
 #
