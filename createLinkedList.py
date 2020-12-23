497e6f73e27a554c8accb962bb9cd32fc157ea36
Damilare Lana <damilarelana@gmail.com>
2020-12-23 13:47:37 +0100

    functional code completed

--- createLinkedList.py
+++ createLinkedList.py
@@ -1,4 +1,5 @@
 import time
+import pdb
 
 # define class LinkedList
 # - representing the head of the linkedlist
@@ -6,53 +7,148 @@ class LinkedList:
     def __init__(self, head=None): # LinkedList instance object initializer
         self.head = head
 
-# mergeIntervals() 
-# - takes in an input list of objects i.e. testList (in the function scope) === tIntervals (in the global scope)
-# - where each object represents a timestamp interval
-# - compares the objects and uses the result to populate another new list
-def mergeIntervals(testList: list):
+    def insertNode(self, data): # data here means value we want to put at the head of the new linkedlist
+        newNode = Node(data)
+        newNode.setNext(self.head) # make the previous head to be the next node to this
+        self.head = newNode # make this the new head
 
-    # check if list is empty and exit accordingly
-    # return the empty list
-    if ((not testList) is True) or (len(testList) == 0) or (testList == None):
-        return testList
+    def getNode(self, data): # data here means value of the node we want
+        currentNode = self.head
+        while not (currentNode is None):
+            if currentNode.getData() == data:
+                return currentNode
+            else: 
+                currentNode = currentNode.getNext()
+        raise ValueError("Data not found")
 
-    newList = list() # initialize a new array
-    newList.append( Interval( # start pre-populating new array with a new first pair as if it is first correct state 
-        getattr(testList[0], 'start'), # extract start value of first interval and pass it here to create a new interval for newArray
-        getattr(testList[0], 'end') # extract end value of first interval and pass it here to create a new interval for newArray
-    ))
+    def countNodes(self, data): # data represents finding how many nodes have that data as their value
+        currentNode = self.head
+        i = 0 # counter for number of nodes
+        while not (currentNode is None):
+            if currentNode.getData() == data:
+                i += 1
+            currentNode = currentNode.getNext() # progress to the next node until the tail tied to None as being the next node
+        return i
 
-    # start iterating over the remaining intervals object in testList
-    for i in range(1, len(testList)):
-        # test the intervals within newList compared to the 
-        lastIndexNewList = len(newList) - 1 # the lastIndex of the newList is constantly changing
-        if getattr(newList[lastIndexNewList],'end') >= getattr(testList[i],'start'): # if endtimestamp of current last interval (of newList) is greater than starttimestamp current test interval of test List, then there is an overlap
-            setattr(newList[lastIndexNewList], 'end', max(testList[i].end, newList[lastIndexNewList].end)), # since there is an overlap, then choose largest endtimestamp (between current last interval [of newList] and current test interval of testList)
-        else: # if there is no overlap, then just append the current test interval object to the newlist
-            newList.append( Interval( 
-                getattr(testList[i], 'start'),
-                getattr(testList[i], 'end')
-            ))
-    return newList # return the now populated newList
-#
-# printListOfObjects()
-def printListOfObjects(listOfIntervalObjects: list):
-    for intervalObject in listOfIntervalObjects:
-        print("[" + str(intervalObject.start) + ", " + str(intervalObject.end) + "]", end=" ") # 'end=" " is used to replace the default \n that comes with print()'
-    return
-# main()
+    def deleteNode(self, data): # deletes all nodes that has data as their value:
+        currentNode = self.head
+        validatedNode = None
+        validationFlag = False
+        while  not (currentNode is None) and (validationFlag is False):
+            if currentNode.getData() == data:
+                validationFlag = True
+            else: 
+                validatedNode = currentNode
+                currentNode = currentNode.getNext()
+        
+        if (currentNode is None): # handles when you get to end of linkedlist or when list is empty
+            raise ValueError("data not found in the LinkedList")
+
+        if (validatedNode is None):
+            self.head = currentNode.getNext()
+        else:
+            validatedNode.setNext(currentNode.getNext())
+        return currentNode
+
+    def listLength(self): # calculates the length of the linkedlist i.e. without comparing any sort of data
+        currentNode = self.head
+        i = 0 # counter to store interations of available nodes
+        while not (currentNode is None):
+            i += 1
+            currentNode = currentNode.getNext()
+        return i
+
+    def printList(self):
+        currentNode = self.head
+        while not (currentNode is None): # iterates through all the notes and prints the data they contain
+            print(currentNode.getData(), end=" ")
+            currentNode = currentNode.getNext()
+
+# define class Node
+# - representing the nodes of the linkedlist
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+    def getData(self):
+        return self.data
+    
+    def getNext(self):
+        return self.next
+    
+    def setNext(self, newNext):
+        self.next = newNext
+
+
+# define createLinkedList()
+#   - takes in a a list with elements being integer values
+def createLinkedList(integerList: list):
+    linkedList = LinkedList() # create shell of linkedlist
+    integerList.reverse() # reverse the list elements in place
+    for i in integerList:   # populate linkedlist in reverse order i.e. least significant number is placed just after the head
+        linkedList.insertNode(i)
+    return linkedList
+
+
+# getInteger()
+# - used to obtain the required test data
+def getIntegers():
+    try:
+        print("using the format 1234 ...")
+        inputIntegerString = input("Enter an integer: ")
+        while (inputIntegerString is None):
+            print("Input cannot be empty")
+            getIntegers() # recursive call
+        try: # check if the input are digits 
+            inputIntegers = int(inputIntegerString)
+            return inputIntegers
+        except ValueError:
+            print("Invalid user define input as integer literal") # handling the exception by calling getIntegers() again
+            getIntegers() # recursive call
+        finally:
+            print("User defined integers, successfully entered")
+    except ValueError:
+        raise Exception("Unable to initialize the user defined integers")
+
+def parseInputIntegers(inputIntegers: int):  # inputString is of type string
+    inputIntegersString = str(inputIntegers)
+    listOfStrings = list(inputIntegersString)
+    listOfInt = [int(s) for s in listOfStrings]  # using list comprehension
+    return listOfInt  # return a list with each integer digit as element of the list
+
+
+# getTestData()
+def getTestData():
+    # obtain 1st test integers
+    print("========================    \n")
+    testIntegerOne = getIntegers()
+    testListOne = parseInputIntegers(testIntegerOne)
+    print("\n1st test Integer: ", testIntegerOne)
+    print("1st test List: ", testListOne)
+    print("========================    \n")
+
+    # obtain 2nd test integer
+    print("Enter the second test integer,", end= " ")
+    testIntegerTwo = getIntegers()
+    testListTwo = parseInputIntegers(testIntegerTwo)
+    print("\n2nd test Integer: ", testIntegerTwo)
+    print("2nd test List: ", testListTwo)
+    print("========================    \n")
+
+    return testListOne, testListTwo
 
 def main():
-
-    # initialize test intervals tInterval
-    tIntervals = [Interval(1, 5), Interval(3, 1), Interval(4,6), Interval(6,8), Interval(10,12), Interval(11,15)] # defines a list of tInterval objects
+    testListOne, testListTwo = getTestData()
 
     start_time = time.time()
-    newMergedList = mergeIntervals(tIntervals)
-    printListOfObjects(newMergedList)
-    print("\nTime: {} seconds".format((time.time() - start_time)))
+    firstLinkedList = createLinkedList(testListOne)
+    secondLinkedList = createLinkedList(testListTwo)
 
+    print("\n1st Linked List is {}".format(firstLinkedList))
+    print("\n2nd Linked List is {}".format(secondLinkedList))
+    print("\nTime: {} seconds".format((time.time() - start_time)))
+    print("    ============    \n")
 
 if __name__ == "__main__":
     main()
